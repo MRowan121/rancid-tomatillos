9c9dcb3eb3201e99296cf2a750b6bb1e27348375
Matt Rowan <matt.rowan121@gmail.com>
2023-03-08 18:13:13 -0500

    Update mpn packages for typescript

--- src/Utilities/apiCalls.tsx
+++ src/Utilities/apiCalls.tsx
@@ -1,13 +1,13 @@
 const url = 'https://rancid-tomatillos.herokuapp.com/api/v2'
 
-const getAllData = (data) => {
-    return fetch(`${url}${data}`)
-    .then(res => {
-        if(!res.ok) {
-            throw new Error('Something went wrong')
-        }
-        return res.json()
-    })
+const getAllData = (urlParams: string) => {
+    return fetch(`${url}${urlParams}`)
+        .then(res => {
+            if(!res.ok) {
+                throw new Error('Something went wrong')
+            }
+            return res.json()
+        })
 }
 
 export { getAllData }
